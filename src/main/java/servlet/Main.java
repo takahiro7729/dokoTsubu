ae987ce3f4448a01a21f02e991d9d17cda0d077b
takahiro <linlonghao61@gmail.com>
2023-09-22 12:22:13 +0900

    つぶやきリストに保存するモデルのインポートを追加

--- src/main/java/servlet/Main.java
+++ src/main/java/servlet/Main.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
+import model.PostMutterLogic;
 import model.Mutter;
 import model.User;
 
