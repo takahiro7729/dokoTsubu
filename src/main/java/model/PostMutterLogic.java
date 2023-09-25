e89fafe880789b7a25eb6da9a6ad61f0f032c9d7
takahiro <linlonghao61@gmail.com>
2023-09-25 15:54:57 +0900

    DAO使用するため仕様変更

--- src/main/java/model/PostMutterLogic.java
+++ src/main/java/model/PostMutterLogic.java
@@ -1,10 +1,10 @@
 package model;
 
-import java.util.List;
+import dao.MuttersDAO;
 
 public class PostMutterLogic {
-	public void execute(Mutter mutter,List <Mutter> mutterList) {
-		mutterList.add(0,mutter); //先頭に追加
-	}
-
+  public void execute(Mutter mutter) { 
+    MuttersDAO dao = new MuttersDAO();
+    dao.create(mutter);
+  }
 }
