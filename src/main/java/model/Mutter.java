f0a2fd18d7a435dd6ce334baccf06860769170c3
takahiro <linlonghao61@gmail.com>
2023-09-25 15:35:25 +0900

    DB接続コード追加

--- src/main/java/model/Mutter.java
+++ src/main/java/model/Mutter.java
@@ -2,14 +2,21 @@ package model;
 import java.io.Serializable;
 
 public class Mutter implements Serializable {
-	private String userName;
-	private String text;
+	private int id;          //ID名
+	private String userName; //ユーザー名
+	private String text;     //つぶやき内容
 
 	public Mutter() {}
 	public Mutter(String userName,String text) {
 		this.userName = userName;
 		this.text = text;
 	}
+	public Mutter(int id, String userName, String text) {
+		this.id = id;
+		this.userName = userName;
+		this.text = text;
+	}
+	public int getId() {return id;}
 	public String getuserName() {return userName;}
 	public String getText() {return text;}
 }
